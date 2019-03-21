bd704c4b77b0d182ec4dccfff508253df1ccfcb2
엄준일 <umc@ntreev.com>
2019-03-21 13:40:37 +0900

    ConsumerGroupStream 과 함께 세마포어 사용 예제 추가

--- src/consumer-group-stream.ts
+++ src/consumer-group-stream.ts
@@ -1,5 +1,8 @@
 import { connect, clientOption } from "./common";
 import { ConsumerGroupStream } from "kafka-node";
+import { Sema } from "async-sema";
+
+const semaphore = new Sema(1);
 
 async function listenConsumerGroupStream(client, topicName): Promise<void> {
     return new Promise<void>((resolve, reject) => {
@@ -7,34 +10,53 @@ async function listenConsumerGroupStream(client, topicName): Promise<void> {
             autoCommit: false,
             groupId: "test-group",
             protocol: ["roundrobin"],
-            kafkaHost: clientOption.kafkaHost
+            kafkaHost: clientOption.kafkaHost,
+            maxTickMessages: 1
         }, topicName)
 
         const read = consumerGroupStream.read(1);
-        consumerGroupStream.on("close", () => {console.log("close");});
-        consumerGroupStream.on("error", (error) => {console.error(error);});
+        consumerGroupStream.on("close", () => { console.log("close"); });
+        consumerGroupStream.on("error", (error) => { console.error(error); });
         consumerGroupStream.on("readable", () => {
             console.log("readable");
             console.log(read);
         });
-        consumerGroupStream.on("data", (chunk) => {
-            console.log("data");
-            console.log(chunk);
-            consumerGroupStream.commit(chunk, true, (error, data) => {
-                if (error) {
-                    console.error(error);
-                    return;
-                }
 
-                console.log("commit");
-                console.log(data);
-            });
+        consumerGroupStream.on("data", async (chunk) => {
+            console.log(chunk.value);
+
+            await semaphore.acquire();
+
+            try {
+                await delay(chunk.value, 2000);
+
+                consumerGroupStream.commit(chunk, true, (error, data) => {
+                    if (error) {
+                        console.error(error);
+                    }
+
+                    console.log("commit");
+                    console.log(data);
+                });
+            }
+            finally {
+                semaphore.release();
+            }
         });
-        consumerGroupStream.on("end", () => {console.log("end");});
+        consumerGroupStream.on("end", () => { console.log("end"); });
     });
 }
 
-(async() => {
+async function delay(message: string, ms: number): Promise<void> {
+    return new Promise<void>(resolve => {
+        setTimeout(() => {
+            console.log(message + " completed");
+            resolve();
+        }, ms);
+    })
+}
+
+(async () => {
     const client = await connect();
     await listenConsumerGroupStream(client, "test");
 })();
